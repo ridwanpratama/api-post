6771ce56eb2b20c1b10e1cff854748b593ea562d
Ridwan <pratamaridwan111@gmail.com>
2021-03-22 18:58:44 +0700

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'title', 'content',
+    ];
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Str;
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+// $router->get('/key', function() {
+//     return Str::random(32);
+// });
+
+$router->group(['prefix' => 'api'], function() use ($router) {
+    $router->get('/posts', 'PostController@index');
+    $router->post('/posts', 'PostController@store');
+    $router->get('/posts/{id}', 'PostController@show');
+    $router->put('/posts/{id}', 'PostController@update');
+    $router->delete('/posts/{id}', 'PostController@destroy');
+});
